1bcba7b639236429922bf7024d21e53ba4994422
Emily Friis <emily-friis@live.nmit.ac.nz>
2021-09-03 13:57:47 +1200

    added comments for route and server js files in _basic

--- _express/_basic/route.js
+++ _express/_basic/route.js
@@ -4,34 +4,34 @@ const http = require("http");
 
 const app = express();
 
-app.get("/", (req, res)=> {
-  res.end("Welcome to my homepage!");
+app.get("/", (req, res)=> { // route path matches requests to the root URL ("/")
+  res.end("Welcome to my homepage!"); // sends message "Welcome to my homepage!"
 });
 
-app.get("/about",(req, res) => {
-  res.end("Welcome to the about page!");
+app.get("/about",(req, res) => { // route path matches requests to "/about"
+  res.end("Welcome to the about page!"); // sends message "Welcome to the about page!"
 });
 
-app.get("/contacts", (req, res)=> {
-  res.end("Welcome to contacts page!");
+app.get("/contacts", (req, res)=> { // route path matches requests to "/contacts"
+  res.end("Welcome to contacts page!"); // sends message "Welcome to contacts page!"
 });
 
-app.get("/hello/:who", (req, res) => {
-  res.end("Hello, " + req.params.who + ".");
+app.get("/hello/:who", (req, res) => { // route path matches requests to "/hello/:who"
+  res.end("Hello, " + req.params.who + "."); // sends message greeting user by route parameter :who
 })
 
-app.get("/redirect_home",(req, res)=> {
-  res.redirect("/");
+app.get("/redirect_home",(req, res)=> { // route path matches requests to "/redirect_home"
+  res.redirect("/"); // redirects to root URL ("/")
 });
 
-app.get("/sendnote", (req, res) => {
-  const filePath = path.resolve(__dirname, "notes.txt");
-  res.sendFile(filePath);
+app.get("/sendnote", (req, res) => { // route path matches requests to "/sendnote"
+  const filePath = path.resolve(__dirname, "notes.txt"); // gets filepath of notes.txt
+  res.sendFile(filePath); // sends the file at file path of notes.txt
 });
 
-app.use((req, res) => {
-  res.statusCode = 404;
-  res.end("404!");
+app.use((req, res) => { // matches requests to any URL
+  res.statusCode = 404; // sets statuscode to 404
+  res.end("404!"); // ends response; sends error message
 });
 
-  http.createServer(app).listen(3000);
+  http.createServer(app).listen(3000); // Starts the server and listens on port 3000 for connections
--- _express/_basic/sever.js
+++ _express/_basic/sever.js
@@ -3,9 +3,9 @@ const http = require("http");
 
 const app = express();
 
-app.use((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello, World!");
+app.use((req, res) => { // matches requests to any URL
+	res.writeHead(200, { "Content-Type": "text/plain" }); // sends a response header to the request with http status code 200 and plain text formatting.
+	res.end("Hello, World!"); // ends response; sending string "Hello, World!" to header
 });
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(3000); // starts the server and listens on port 3000 for connections
